9cbdd155fa0466215ecc91406fec72d8a20be018
vincentbwalker <vincethewalker@gmail.com>
2019-06-04 12:33:32 +0100

    First Commit

---
+++ app/controllers/page.rb
@@ -0,0 +1,35 @@
+JobVacancy::App.controllers :page do
+  
+  get :about, :map => '/about' do
+    render :erb, 'about'
+  end
+
+  get :contact , :map => '/contact' do
+    render :erb, 'contact'
+  end
+
+  get :home, :map => '/' do
+    render :erb, 'home'
+  end
+  
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   "Maps to url '/foo/#{params[:id]}'"
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+
+end

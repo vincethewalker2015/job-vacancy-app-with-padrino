c3ac6d30cff11b618ac96c65d20cbe350770b5b9
vincentbwalker <vincethewalker@gmail.com>
2019-06-06 16:09:00 +0100

    Added 'user' and 'job_offer' tables and models

---
+++ app/models/job_offer.rb
@@ -0,0 +1,3 @@
+class JobOffer < ActiveRecord::Base
+    belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "job_offers", force: :cascade do |t|
+    t.string "title"
+    t.string "location"
+    t.text "description"
+    t.string "contact"
+    t.date "time_start"
+    t.date "time_end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
